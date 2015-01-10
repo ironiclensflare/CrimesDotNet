2bc00504f97537dd3441de5ea52e0e2364386b08
Stuart Carruthers <stu@ironiclensflare.com>
2015-01-10 11:51:02 +0000

    Started on interface

---
+++ CrimesDotNet/ICrimeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimesDotNet
+{
+    interface ICrimeService
+    {
+        List<CrimeDataset> GetAvailableDatasets();
+        List<Crime> GetCrimes(double lat, double lng);
+        List<Crime> GetCrimes(double lat, double lng, string date);
+        List<Crime> GetCrimes(double lat, double lng, CrimeDataset date);
+        List<Crime> GetCrimes(Poly poly);
+        List<Crime> GetCrimes(Poly poly, string date);
+        List<Crime> GetCrimes(Poly poly, CrimeDataset date);
+    }
+}
---
+++ CrimesDotNet/Poly.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimesDotNet
+{
+    public class Poly
+    {
+        public Poly(LatLng[] points)
+        {
+            Points = points;
+        }
+
+        public Poly()
+        {
+
+        }
+
+        public LatLng[] Points { get; set; }
+
+        public override string ToString()
+        {
+            string output = "";
+
+            for (int i = 0; i < Points.Length; i++)
+            {
+                output += String.Format("{0},{1}", Points[i].Lat, Points[i].Lng);
+
+                if (i < Points.Length - 1)
+                {
+                    output += ":";
+                }
+            }
+
+            return output;
+        }
+    }
+
+    public class LatLng
+    {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+    }
+}
