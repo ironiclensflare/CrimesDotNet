82d6979b1a58c3c91f5fecd5ab19ade0b70e80fb
Stuart Carruthers <stu@ironiclensflare.com>
2015-01-10 13:13:54 +0000

    It works!

--- CrimesDotNet/Crime.cs
+++ CrimesDotNet/Crime.cs
@@ -1,13 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace CrimesDotNet
 {
     public class Crime
     {
+        [JsonProperty("category")]
+        public string Category { get; set; }
 
+        [JsonProperty("persistent_id")]
+        public string PersistentId { get; set; }
+
+        [JsonProperty("location_type")]
+        public LocationType LocationType { get; set; }
+
+        [JsonProperty("location_subtype")]
+        public string LocationSubtype { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+
+        [JsonProperty("context")]
+        public string Context { get; set; }
+
+        [JsonProperty("month")]
+        public string Month { get; set; }
+
+        [JsonProperty("outcome_status")]
+        public OutcomeStatus OutcomeStatus { get; set; }
+    }
+
+    public class Location
+    {
+        [JsonProperty("latitude")]
+        public double Lat { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Lng { get; set; }
+
+        [JsonProperty("street")]
+        public Street Street { get; set; }
+    }
+
+    public class Street
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class OutcomeStatus
+    {
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+    }
+
+    public enum LocationType
+    {
+        Force,
+        BTP
     }
 }
--- CrimesDotNet/CrimeService.cs
+++ CrimesDotNet/CrimeService.cs
@@ -1,13 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CrimesDotNet
 {
     public class CrimeService : ICrimeService
     {
-        
+        private CrimeWebCalls web = new CrimeWebCalls();
+
+        public List<CrimeDataset> GetAvailableDatasets()
+        {
+            string url = "http://data.police.uk/api/crimes-street-dates";
+            string json = web.MakeHttpGetRequest(url);
+            List<CrimeDataset> output = JsonConvert.DeserializeObject<List<CrimeDataset>>(json);
+            return output;
+        }
+
+        public List<Crime> GetCrimes(double lat, double lng)
+        {
+            string url = String.Format("http://data.police.uk/api/crimes-street/all-crime?lat={0}&lng={1}", lat, lng);
+            List<Crime> output = web.RetrieveCrimes(url);
+            return output;
+        }
+
+        public List<Crime> GetCrimes(double lat, double lng, string date)
+        {
+            string url = String.Format("http://data.police.uk/api/crimes-street/all-crime?lat={0}&lng={1}&date={2}", lat, lng, date);
+            List<Crime> output = web.RetrieveCrimes(url);
+            return output;
+        }
+
+        public List<Crime> GetCrimes(double lat, double lng, CrimeDataset date)
+        {
+            List<Crime> output = GetCrimes(lat, lng, date.Date);
+            return output;
+        }
+
+        public List<Crime> GetCrimes(Poly poly)
+        {
+            string url = String.Format("http://data.police.uk/api/crimes-street/all-crime?poly={0}", poly.ToString());
+            List<Crime> output = web.RetrieveCrimes(url);
+            return output;
+        }
+
+        public List<Crime> GetCrimes(Poly poly, string date)
+        {
+            string url = String.Format("http://data.police.uk/api/crimes-street/all-crime?poly={0}&date={1}", poly.ToString(), date);
+            List<Crime> output = web.RetrieveCrimes(url);
+            return output;
+        }
+
+        public List<Crime> GetCrimes(Poly poly, CrimeDataset date)
+        {
+            List<Crime> output = GetCrimes(poly, date.Date);
+            return output;
+        }
     }
 }
